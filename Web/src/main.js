4a050f41bb3d88aab85f946e3a8406edb64fc5f3
Echokovo <hwk13712775583@gmail.com>
2025-07-04 01:46:44 +0800

    Fixed conflict

--- Web/src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import './assets/styles/main.css'
-import './assets/styles/global-gradient.css' // 引入全局渐变背景样式
-
-const app = createApp(App)
-app.use(router)
-app.mount('#app')
